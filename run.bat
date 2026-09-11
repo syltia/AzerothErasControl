945438715e8a72ba40f80cb0cc809a6354e7132f
syltia <xrank1frx@gmail.com>
2026-09-11 15:00:36 +0200

    Use tkinterdnd2 for Explorer drag/drop

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
 
-py -c "import customtkinter, paramiko, windnd" >nul 2>&1
+py -c "import customtkinter, paramiko, tkinterdnd2" >nul 2>&1
 if errorlevel 1 (
     echo Installation des dependances...
     py -m pip install -r requirements.txt
